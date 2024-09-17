a9d66220acde18bf570faa44e66091c9403b25c1
geb <853934146@qq.com>
2024-09-17 23:40:08 +0800

    feat: add user feature

--- src/lib/types/User.ts
+++ src/lib/types/User.ts
@@ -11,4 +11,10 @@ export interface User extends Timestamps {
 	hfUserId: string;
 	isAdmin?: boolean;
 	isEarlyAccess?: boolean;
+
+	// 新增字段
+	proExpiration?: Date; // 会员到期时间
+	tokenBalance?: number; // Token 余额
+	totalRechargeAmount?: number; // 总充值金额
+	apiKey?: string; // API Key
 }
